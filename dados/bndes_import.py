cbdcb09ca57685393e5f50439e149e778b45a2ec
Alê Borba <alexandre.borba@imasters.com.br>
2013-07-13 12:36:25 -0300

    Suporte ao ano de 2011 no script de importação.

--- dados/bndes_import.py
+++ dados/bndes_import.py
@@ -43,6 +43,41 @@ if ano == '2012':
 
 if ano == '2011':
     print ano
+    
+    empresa = ''
+    cnpj = ''
+    area = ''
+
     for dados in data:
-        if dados[5] != '' and dados[1] != 'CNPJ':
-            print dados
+        if dados[0] != '' and dados[5] != '' and dados[1] != 'CNPJ':
+            empresa = dados[0]
+            cnpj = dados[1]
+            area = dados[6]
+
+            valor_split = dados[5].split('.')
+            valor_contrato = ''.join(valor_split)
+
+            new_data.append({'empresa' : unicode(dados[0], "ISO-8859-1"),
+                             'cnpj' : unicode(dados[1], "ISO-8859-1"),
+                             'desc_projeto' : unicode(dados[2].strip(), "ISO-8859-1"),
+                             'uf' : unicode(dados[3], "ISO-8859-1"),
+                             'data_contrato' : unicode(dados[4], "ISO-8859-1"),
+                             'valor_contrato' : int(valor_contrato),
+                             'area' : unicode(dados[6], "ISO-8859-1")})
+    
+        elif dados[0] == '' and dados[2] != '':
+
+            valor_split = dados[5].split('.')
+            valor_contrato = ''.join(valor_split)
+
+            new_data.append({'empresa' : unicode(empresa, "ISO-8859-1"),
+                             'cnpj' : unicode(cnpj, "ISO-8859-1"),
+                             'desc_projeto' : unicode(dados[2].strip(), "ISO-8859-1"),
+                             'uf' : unicode(dados[3], "ISO-8859-1"),
+                             'data_contrato' : unicode(dados[4], "ISO-8859-1"),
+                             'valor_contrato' : int(valor_contrato),
+                             'area' : unicode(area, "ISO-8859-1")})
+
+    for item in new_data:
+        new_item = json.dumps(item)
+        bndes.doismileonze.insert(eval(new_item))
